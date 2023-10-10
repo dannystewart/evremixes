a0e46c04911507f71097e92b364f5e23469859f9
Danny Stewart <danny@stewart.cc>
2023-10-10 17:13:49 -0400

    add downloader for the downloader

---
+++ evremixes-downloader.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Create a temporary directory
+temp_dir=$(mktemp -d -t evremixes-XXXXXX)
+
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+    echo -e "${RED}Error:${NC} curl is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Fancy download with user feedback
+echo -e "${GREEN}Downloading evremixes...${NC}"
+if curl -o "${temp_dir}/evremixes" -L "https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/evremixes" --progress-bar; then
+    echo -e "${GREEN}Download complete.${NC}"
+else
+    echo -e "${RED}Download failed.${NC}"
+    rm -r "${temp_dir}"
+    exit 1
+fi
+
+# Make the file executable
+chmod +x "${temp_dir}/evremixes"
+
+# Run the program
+echo -e "${GREEN}Running evremixes...${NC}"
+"${temp_dir}/evremixes"
+
+# Clean up by removing the temporary directory
+echo -e "${GREEN}Cleaning up...${NC}"
+rm -r "${temp_dir}"
+echo -e "${GREEN}Done!${NC}"
