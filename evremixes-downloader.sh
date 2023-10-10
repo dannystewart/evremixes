90f510cde201656bcc30127d0e4b675dcdb66381
Danny Stewart <danny@stewart.cc>
2023-10-10 18:38:45 -0400

    architecture support (arm first)

--- evremixes-downloader.sh
+++ evremixes-downloader.sh
@@ -8,6 +8,20 @@ NC='\033[0m' # No Color
 # Create a temporary directory
 temp_dir=$(mktemp -d -t evremixes-XXXXXX)
 
+# Determine architecture
+ARCH=$(uname -m)
+URL=""
+
+if [ "$ARCH" == "x86_64" ]; then
+    URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/x86/evremixes"
+elif [ "$ARCH" == "arm64" ]; then
+    URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/arm/evremixes"
+else
+    echo -e "${RED}Error:${NC} Unsupported architecture."
+    rm -r "${temp_dir}"
+    exit 1
+fi
+
 # Check if curl is installed
 if ! command -v curl &> /dev/null; then
     echo -e "${RED}Error:${NC} curl is not installed. Please install it and try again."
@@ -16,7 +30,7 @@ fi
 
 # Fancy download with user feedback
 echo -e "${GREEN}Downloading evremixes...${NC}"
-if curl -o "${temp_dir}/evremixes" -L "https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/evremixes" --progress-bar; then
+if curl -o "${temp_dir}/evremixes" -L "$URL" --progress-bar; then
     echo -n ""
 else
     echo -e "${RED}Download failed.${NC}"
