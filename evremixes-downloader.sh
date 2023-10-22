531e1ea5e1d460d20511cbecfe533fc34f08600b
Danny Stewart <danny@stewart.cc>
2023-10-22 13:16:33 -0400

    have Python downloader fallback to Bash

--- evremixes-downloader.sh
+++ evremixes-downloader.sh
@@ -1,68 +1,198 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
-# Define colors for output
-RED='\033[0;31m'
+# Color codes
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# Create a temporary directory
-temp_dir=$(mktemp -d -t evremixes-XXXXXX)
+# Dependency check
+check_dependencies() {
+    dependency_met=true
 
-# Check if ffmpeg is installed
-if ! command -v ffmpeg &> /dev/null; then
-    echo -e "${RED}Warning:${NC} ffmpeg is not installed."
-    # Check if Homebrew is installed
-    if command -v brew &> /dev/null; then
-        echo -e "${GREEN}Homebrew is installed, attempting to install ffmpeg...${NC}"
-        brew install ffmpeg
-        if [ $? -ne 0 ]; then
-            echo -e "${RED}Failed to install ffmpeg.${NC}"
-            rm -r "${temp_dir}"
-            exit 1
+    # Check if ffmpeg is installed
+    if ! command -v ffmpeg &>/dev/null; then
+        echo -e "${RED}Warning:${NC} ffmpeg is not installed."
+        dependency_met=false
+    fi
+
+    # Check if curl is installed
+    if ! command -v curl &>/dev/null; then
+        echo -e "${RED}Error:${NC} curl is not installed."
+        dependency_met=false
+    fi
+}
+
+# Spinner function
+spin() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    tput civis # Hide cursor
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    tput cnorm # Restore cursor
+    printf "    \b\b\b\b"
+}
+
+# Cleanup function to remove temp files
+cleanup() {
+    kill_switch=1
+    if [ -d "$output_folder" ]; then
+        find "$output_folder" -name "*.m4a.temp" -type f -exec rm -f {} +
+        if [ "$(find "$output_folder" -maxdepth 0 -empty 2>/dev/null)" ]; then
+            rmdir "$output_folder"
         fi
+    fi
+}
+
+# New trap function to only set kill_switch for SIGINT
+ctrl_c() {
+    kill_switch=1
+    echo ""
+    echo -e "${RED}Script aborted. Exiting.${NC}"
+    exit 1
+}
+
+# Trap to call cleanup function on EXIT, SIGTERM
+trap cleanup EXIT SIGTERM
+
+# Separate Trap to call ctrl_c function on SIGINT
+trap ctrl_c SIGINT
+
+check_dependencies
+
+if [ "$dependency_met" = true ]; then
+    # Create a temporary directory
+    temp_dir=$(mktemp -d -t evremixes-XXXXXX)
+
+    # Determine architecture
+    ARCH=$(uname -m)
+    URL=""
+
+    if [ "$ARCH" == "x86_64" ]; then
+        URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/x86/evremixes"
+    elif [ "$ARCH" == "arm64" ]; then
+        URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/arm/evremixes"
     else
-        echo -e "${RED}Error:${NC} Homebrew is not installed. Cannot install ffmpeg automatically."
+        echo -e "${RED}Error:${NC} Unsupported architecture."
         rm -r "${temp_dir}"
         exit 1
     fi
-fi
 
-# Determine architecture
-ARCH=$(uname -m)
-URL=""
+    # Fancy download with user feedback
+    echo -e "${GREEN}Downloading evremixes...${NC}"
+    if curl -o "${temp_dir}/evremixes" -L "$URL" --progress-bar; then
+        echo -n ""
+    else
+        echo -e "${RED}Download failed.${NC}"
+        rm -r "${temp_dir}"
+        exit 1
+    fi
 
-if [ "$ARCH" == "x86_64" ]; then
-    URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/x86/evremixes"
-elif [ "$ARCH" == "arm64" ]; then
-    URL="https://git.dannystewart.com/danny/evremixes/raw/branch/main/dist/arm/evremixes"
-else
-    echo -e "${RED}Error:${NC} Unsupported architecture."
+    # Make the file executable
+    chmod +x "${temp_dir}/evremixes"
+
+    # Run the program
+    echo -e "${GREEN}Running evremixes...${NC}"
+    "${temp_dir}/evremixes"
+
+    # Clean up by removing the temporary directory
     rm -r "${temp_dir}"
-    exit 1
-fi
-
-# Check if curl is installed
-if ! command -v curl &> /dev/null; then
-    echo -e "${RED}Error:${NC} curl is not installed. Please install it and try again."
-    exit 1
-fi
-
-# Fancy download with user feedback
-echo -e "${GREEN}Downloading evremixes...${NC}"
-if curl -o "${temp_dir}/evremixes" -L "$URL" --progress-bar; then
-    echo -n ""
 else
-    echo -e "${RED}Download failed.${NC}"
-    rm -r "${temp_dir}"
-    exit 1
+    output_folder="$HOME/Downloads/Evanescence Remixes"
+    kill_switch=0
+
+    # Welcome message
+    echo ""
+    echo -e "${GREEN}Saving Evanescence Remixes to ~/Downloads...${NC}"
+
+    # Fetch JSON and store it in a variable, sorting tracks by track_number
+    json_data=$(curl -s "https://git.dannystewart.com/danny/evremixes/raw/branch/main/evtracks.json" | python3 -c "import sys, json; data=json.load(sys.stdin); data['tracks'] = sorted(data['tracks'], key=lambda x: x['track_number']); print(json.dumps(data))")
+
+    # Create output folder if it doesn't exist, and handle old files if it does
+    if [ -d "$output_folder" ]; then
+        find "$output_folder" \( -name "*.m4a" -o -name "*.m4a.temp" \) -type f -exec rm -f {} +
+        echo -e "${YELLOW}Folder already exists; older files removed.${NC}"
+    else
+        mkdir -p "$output_folder"
+    fi
+
+    echo ""
+
+    # Loop over each track in the JSON array
+    length=$(echo "$json_data" | python3 -c "import sys, json; print(len(json.load(sys.stdin)['tracks']))")
+    for i in $(seq 0 $(($length - 1))); do
+
+        if [ $kill_switch -eq 1 ]; then
+            echo -e "${RED}Script aborted. Exiting.${NC}"
+            exit 1
+        fi
+
+        export INDEX=$i
+        track_name=$(echo "$json_data" | python3 -c "import sys, json, os; data=json.load(sys.stdin); i=int(os.environ['INDEX']); print(data['tracks'][i]['track_name'])")
+        file_url=$(echo "$json_data" | python3 -c "import sys, json, os; data=json.load(sys.stdin); i=int(os.environ['INDEX']); print(data['tracks'][i]['file_url'])")
+        track_number=$(echo "$json_data" | python3 -c "import sys, json, os; data=json.load(sys.stdin); i=int(os.environ['INDEX']); print(data['tracks'][i]['track_number'])")
+
+        # Create new .m4a URL
+        m4a_url=${file_url/%.flac/.m4a}
+
+        # Generate final and temporary file names
+        formatted_track_number=$(printf "%02d" "$track_number")
+        temp_filename="$output_folder/$formatted_track_number - $track_name.m4a.temp"
+        final_filename="$output_folder/$formatted_track_number - $track_name.m4a"
+
+        # Initialize retry counter
+        retry_count=0
+        max_retries=5
+
+        # Use curl to download the file
+        echo -n "[$((i + 1))/$length] Downloading ${track_name}..."
+
+        # Download loop with retry logic
+        while [ $retry_count -lt $max_retries ]; do
+            if [ $kill_switch -eq 1 ]; then
+                echo -e "${RED}Script aborted. Exiting.${NC}"
+                exit 1
+            fi
+
+            curl --fail-early -s "$m4a_url" -o "$temp_filename" &
+            CURL_PID=$!
+            spin $CURL_PID
+            wait $CURL_PID
+            CURL_EXIT_STATUS=$?
+
+            if [ $CURL_EXIT_STATUS -eq 0 ]; then
+                break
+            fi
+
+            # Increment retry counter if the download failed
+            ((retry_count++))
+
+            # Introduce a slight delay between attempts
+            sleep 1
+        done
+
+        # Overwrite the line depending on success or failure
+        if [ $CURL_EXIT_STATUS -ne 0 ]; then
+            echo -e "\r${RED}Download failed for ${track_name}${NC}            " # Extra spaces to clear the spinner
+        else
+            mv "$temp_filename" "$final_filename"
+            echo -e "\r${GREEN}✔ ${track_name}${NC}                            " # Extra spaces to clear the spinner
+        fi
+    done
+
+    echo ""
+    echo -e "${GREEN}All tracks downloaded! Enjoy!${NC}"
+
+    # Open in Finder if we can
+    if [ "$(command -v open)" ]; then
+        open "$output_folder"
+    fi
 fi
-
-# Make the file executable
-chmod +x "${temp_dir}/evremixes"
-
-# Run the program
-echo -e "${GREEN}Running evremixes...${NC}"
-"${temp_dir}/evremixes"
-
-# Clean up by removing the temporary directory
-rm -r "${temp_dir}"
