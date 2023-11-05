8bc91c118d72377a7d48e55e9213cdfa83c18655
Danny Stewart <danny@stewart.cc>
2023-11-05 10:58:33 -0500

    cleanup, comments, fix M4A content type

--- tools/evazure.py
+++ tools/evazure.py
@@ -24,9 +24,8 @@ from termcolor import colored
 # Initialize spinner, temp dir, env vars, and Azure client
 spinner = Halo(text="Initializing", spinner="dots")
 temp_dir = tempfile.mkdtemp()
-env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
 load_dotenv()
-load_dotenv(dotenv_path=env_path)
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))
 connection_string = os.getenv("AZURE_CONNECTION_STRING")
 if not connection_string:
     print(colored("Error: AZURE_CONNECTION_STRING not found. Check environment variables.", "red"))
@@ -34,22 +33,26 @@ if not connection_string:
 container_client = BlobServiceClient.from_connection_string(connection_string).get_container_client("music")
 
 
-# Retrieve and prepare metadata
+# Retrieve and prepare metadata, download cover art
 def fetch_metadata():
     spinner.start(colored("Downloading track metadata...", "cyan"))
+
     response = requests.get("https://git.dannystewart.com/danny/evremixes/raw/branch/main/evtracks.json")
     track_data = json.loads(response.text)
-    cover_data = (
-        Image.open(BytesIO(requests.get(track_data.get("metadata", {}).get("cover_art_url", "")).content))
-        .convert("RGB")
-        .resize((800, 800))
-    )
+
+    cover_art_url = track_data.get("metadata", {}).get("cover_art_url", "")
+    cover_art_response = requests.get(cover_art_url)
+    cover_art_bytes = BytesIO(cover_art_response.content)
+    cover_image = Image.open(cover_art_bytes).convert("RGB")
+    cover_data = cover_image.resize((800, 800))
     buffered = BytesIO()
     cover_data.save(buffered, format="JPEG")
-    return track_data.get("metadata", {}), track_data, buffered.getvalue()
+    cover_buffered = buffered.getvalue()
+
+    return track_data.get("metadata", {}), track_data, cover_buffered
 
 
-# Identify track based on downloaded metadata
+# Identify track by matching upload filename against URLs in metadata
 def get_track_metadata(filename, track_data):
     for track in track_data.get("tracks", []):
         json_filename, _ = os.path.splitext(os.path.basename(track["file_url"]))
@@ -59,20 +62,26 @@ def get_track_metadata(filename, track_data):
 
 
 # Handle audio conversion
-def convert_audio(input_file, file_format):
+def convert_audio_file(input_file, file_format):
     global temp_dir
+
+    # Identify the desired file format and then process accordingly
     wav_audio = AudioSegment.from_file(input_file, format="wav")
     audio_file_path = os.path.join(temp_dir, os.path.basename(input_file).replace(".wav", f".{file_format}"))
+
     if file_format == "flac":
         wav_audio.export(audio_file_path, format="flac")
     elif file_format == "m4a":
         wav_audio.export(audio_file_path, format="ipod", codec="alac")
+
     return audio_file_path
 
 
 # Add metadata to files
-def add_metadata(audio_file_path, album_metadata, track_metadata):
+def add_metadata_to_file(audio_file_path, album_metadata, track_metadata):
     file_format = os.path.splitext(audio_file_path)[1][1:].lower()
+
+    # Handle FLAC-specific metadata
     if file_format == "flac":
         audio = FLAC(audio_file_path)
         audio["title"] = track_metadata.get("track_name", "")
@@ -88,6 +97,8 @@ def add_metadata(audio_file_path, album_metadata, track_metadata):
         img.mime = "image/jpeg"
         img.desc = "Cover (front)"
         audio.add_picture(img)
+
+    # Handle M4A-specific metadata
     elif file_format == "m4a":
         audio = MP4(audio_file_path)
         audio["\xa9nam"] = track_metadata.get("track_name", "")
@@ -98,33 +109,51 @@ def add_metadata(audio_file_path, album_metadata, track_metadata):
         audio["\xa9gen"] = album_metadata.get("genre", "")
         audio["\xa9day"] = str(album_metadata.get("year", ""))
         audio["covr"] = [MP4Cover(cover_data, imageformat=MP4Cover.FORMAT_JPEG)]
+
     audio.save()
     return audio_file_path
 
 
 # Process and upload files
-def process_and_upload(input_file, album_metadata, track_metadata, blob_name, file_format):
+def process_and_upload_file(input_file, album_metadata, track_metadata, blob_name, file_format):
     spinner.start(colored(f"Converting to {file_format.upper()} and adding metadata...", "cyan"))
-    converted_file_path = add_metadata(convert_audio(input_file, file_format), album_metadata, track_metadata)
-    content_type = f"audio/{file_format.lower()}"
+
+    # Convert file to specified format and add metadata
+    converted_file = convert_audio_file(input_file, file_format)
+    converted_file_with_metadata = add_metadata_to_file(converted_file, album_metadata, track_metadata)
+
+    # Set content type for Azure (this makes it playable in browsers)
+    content_type = "audio/mp4" if file_format.lower() == "m4a" else "audio/flac"
     content_settings = ContentSettings(content_type=content_type)
+
     spinner.start(colored(f"Uploading {file_format.upper()} to Azure...", "cyan"))
+
+    # Upload file to Azure
     blob_client = container_client.get_blob_client(f"ev/{blob_name}.{file_format}")
-    with open(converted_file_path, "rb") as data:
+    with open(converted_file_with_metadata, "rb") as data:
         blob_client.upload_blob(data, content_settings=content_settings, overwrite=True)
+
     spinner.succeed(colored(f"{file_format.upper()} tagged and uploaded!", "green"))
 
 
-# Purge Azure CDN cache
-def purge_cdn_cache():
-    process = subprocess.run(
-        "az cdn endpoint purge --resource-group dsfiles --name dsfiles --profile-name dsfiles --content-paths /ev",
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    if process.returncode != 0:
-        raise Exception(f"Failed to purge Azure CDN cache. Error: {process.stderr.decode('utf-8')}")
+# Purge the Azure CDN cache
+def purge_azure_cdn_cache():
+    try:
+        spinner.start(colored("Purging CDN cache (takes a while, Ctrl-C to skip)...", "cyan"))
+        process = subprocess.run(
+            "az cdn endpoint purge --resource-group dsfiles --name dsfiles --profile-name dsfiles --content-paths /ev",
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if process.returncode != 0:
+            raise Exception(f"Failed to purge Azure CDN cache. Error: {process.stderr.decode('utf-8')}")
+
+        spinner.succeed(colored("CDN cache purged!", "green"))
+    except KeyboardInterrupt:
+        spinner.warn(colored("CDN purge skipped.", "yellow"))
+    except Exception as e:
+        spinner.fail(f"Failed to purge CDN: {e}")
 
 
 # Main function
@@ -133,7 +162,8 @@ def main(filename, input_file):
     album_metadata, track_data, cover_data = fetch_metadata()
     try:
         blob_name, _ = os.path.splitext(filename)
-        spinner.start(colored("Starting processing of audio files...", "cyan"))
+
+        # Try to identify the track being uploaded
         spinner.start(colored("Matching track metadata...", "cyan"))
         track_metadata = get_track_metadata(filename, track_data)
         if not track_metadata:
@@ -142,20 +172,19 @@ def main(filename, input_file):
         track_matched = track_metadata.get("track_name", "")
         album_metadata = track_data.get("metadata", {})
         spinner.succeed(colored(f"Track matched: {track_matched}", "green"))
-        process_and_upload(input_file, album_metadata, track_metadata, blob_name, "flac")
-        process_and_upload(input_file, album_metadata, track_metadata, blob_name, "m4a")
+
+        # Process and upload the FLAC version then the M4A version
+        process_and_upload_file(input_file, album_metadata, track_metadata, blob_name, "flac")
+        process_and_upload_file(input_file, album_metadata, track_metadata, blob_name, "m4a")
+
+        # Purge the Azure CDN cache so the new files are available
+        purge_azure_cdn_cache()
     except Exception as e:
         spinner.fail(colored(f"An error occurred: {str(e)}", "red"))
-    try:
-        spinner.start(colored("Purging CDN cache (takes a while, Ctrl-C to skip)...", "cyan"))
-        purge_cdn_cache()
-        spinner.succeed(colored("CDN cache purged!", "green"))
-    except KeyboardInterrupt:
-        spinner.warn(colored("CDN purge skipped.", "yellow"))
-    except Exception as e:
-        spinner.fail(f"Failed to purge CDN: {e}")
+
     spinner.succeed(colored("All done!", "green"))
 
+    # Copy FLAC URL to clipboard and output both URLs
     flac_url = f"https://files.dannystewart.com/music/ev/{blob_name}.flac"
     m4a_url = f"https://files.dannystewart.com/music/ev/{blob_name}.m4a"
     pyperclip.copy(flac_url)
@@ -165,6 +194,7 @@ def main(filename, input_file):
 
 
 if __name__ == "__main__":
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="Upload and convert audio file to Azure Blob Storage.")
     parser.add_argument(
         "input_file",
@@ -180,14 +210,20 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     _, ext = os.path.splitext(args.input_file)
+
+    # Make sure the input file is a WAV or AIFF file
     if ext.lower() not in [".wav", ".aif", ".aiff"]:
         print("Invalid file type. Please provide a WAV or AIFF file.")
         sys.exit(1)
+
+    # If no upload filename is given, use the input filename minus the version number
     if args.filename is None:
         args.filename = re.sub(
             r"\s*\d+\.\d+\.\d+(_\d+)?", "", os.path.splitext(os.path.basename(args.input_file))[0]
         ).replace(" ", "-")
         print(colored(f"No filename given, uploading as {args.filename}.flac", "cyan"))
+
+    # Run the main function, aborting on Ctrl-C
     try:
         main(args.filename, args.input_file)
     except KeyboardInterrupt:
